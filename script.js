6c483f5161eedf6a14e5cf5cb7a3b757bdf2e44c
Jonatas Andrade <106789706+Jonatas1411@users.noreply.github.com>
2022-06-13 23:37:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "Jonatas";
+let sobrenome = "Andrade";
+let anoDeNascimento = 2000;
+let minhaCidade = "Salvador";
+let anoAtual = 2022;
+let idade = 22
+console .log(`Eu sou o ${nome} ${sobrenome}, tenho ${idade} anos e moro em ${minhaCidade}`)
+console.log(anoAtual-anoDeNascimento)
